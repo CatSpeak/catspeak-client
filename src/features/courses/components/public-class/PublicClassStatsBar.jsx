6ce20ec75644bc50cccef8b16dd92929dcdbb459
helitoo <bao162006@gmail.com>
2026-08-24 00:36:35 +0700

    Update session hour UI

--- src/features/courses/components/public-class/PublicClassStatsBar.jsx
+++ src/features/courses/components/public-class/PublicClassStatsBar.jsx
@@ -37,7 +37,7 @@ const PublicClassStatsBar = ({ classData }) => {
   const endTime = firstSchedule?.endTime || classData?.endTime
   const timeFormatted =
     startTime && endTime
-      ? `${formatScheduleTime(startTime)} - ${formatScheduleTime(endTime)}`
+      ? `${formatScheduleTime(startTime)} → ${formatScheduleTime(endTime)}`
       : startTime
         ? formatScheduleTime(startTime)
         : null
